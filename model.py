b25e3cdf30e90b25454a42f0f3a9892866a76da7
RKThompson <robertkellythompson@gmail.com>
2022-05-19 15:41:23 -0400

    Added can_allocate function to model.py to return False if qty cannot allocate.

--- model.py
+++ model.py
@@ -19,16 +19,40 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: set[OrderLine]
 
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+
+'''
     def allocate(self, line: OrderLine):
         if(self.check_in_stock(line)):
             self.available_quantity -= line.qty
         else:
             print('Out of stock')
 
+
     def check_in_stock(self, line):
         if(self.available_quantity >= line.qty):
             return True
         else:
             return False
+'''

67a07f13788289eadf9a3191d6589d576bbb7a2a
RKThompson <robertkellythompson@gmail.com>
2022-05-24 15:20:35 -0400

    Added OutOfStock exception to model.py and a test for the allocation into test_allocate.py

--- model.py
+++ model.py
@@ -5,11 +5,14 @@ from datetime import date
 
 
 def allocate(line: OrderLine, batches: List[Batch]) -> str:
-    batch = next(
-        b for b in sorted(batches) if b.can_allocate(line)
-    )
-    batch.allocate(line)
-    return batch.reference
+    try:
+        batch = next(
+            b for b in sorted(batches) if b.can_allocate(line)
+        )
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f'Out of stock for sku {line.sku}')
 
 
 # Order class
@@ -64,3 +67,8 @@ class Batch:
 
     def __hash__(self):
         return hash(self.reference)
+
+
+# exception class
+class OutOfStock(Exception):
+    pass
